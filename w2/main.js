a74f6f15d34c0864fb6f7efbcc55a86a0733cf96
Gio559 <joba2017@outlook.com>
2024-01-18 19:04:27 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,14 @@
+const houseMmbers = 6;
+const sizeHome = 4;
+const foodChoice = 12;
+const waterUsage = 2;
+const housePurch = 6;
+const wasteProduce = 20;
+const recycle = 16;
+const vehicaleUsage = 10;
+
+const total = houseMmbers + sizeHome + foodChoice +
+waterUsage + housePurch + wasteProduce + recycle +
+vehicaleUsage; 
+const myHeading = document.querySelector("h2");
+myHeading.textContent = 76; 
