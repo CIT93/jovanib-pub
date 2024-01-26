d903961135bd16bf3cc9a1853ce6e754e3e666d5
Gio559 <joba2017@outlook.com>
2024-01-25 23:32:22 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -22,5 +22,22 @@ function determineHouseHoldPts (numberInHousehold) {
 let carbonFpPoints = 0;
 // const numberInHousehold = 5;
 // global scope 
-determineHouseHoldPts (3)
-determineHouseHoldPts (4)
+// determineHouseHoldPts (3) 
+// determineHouseHoldPts (4)
+// 
+
+function calculateEnviornmentPoints (homeSize) {
+    if (homeSize === "large") {
+      return 10;
+    } else if (homeSize === "medium") {
+      return 7;
+    } else if (homeSize === "small") {
+      return 4;
+    } else if (homeSize === "apt") {
+      return 2;
+    }
+  }
+  let homeSize = "medium";
+  let enviornmentPoints = calculateEnviornmentPoints(homeSize);
+  
+  console.log(`My home is ${homeSize} sized and my points are ${enviornmentPoints}.`)
