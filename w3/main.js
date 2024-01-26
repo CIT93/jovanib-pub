e2faa8de9325a8a903c4a7d2384ebccc819b348c
Gio559 <joba2017@outlook.com>
2024-01-25 22:37:21 -0800

    setup done

---
+++ w3/main.js
@@ -0,0 +1,20 @@
+let carbonFpPoints = 0;
+const numberInHousehold = 5;
+if (numberInHousehold === 1) {
+    carbonFpPoints = carbonFpPoints + 14
+} else if (numberInHousehold === 2) {
+    carbonFpPoints = carbonFpPoints + 12
+} else if (numberInHousehold === 3) {
+    carbonFpPoints = carbonFpPoints + 10
+} else if (numberInHousehold === 4) {
+    carbonFpPoints = carbonFpPoints + 8
+} else if (numberInHousehold === 5) {
+    carbonFpPoints = carbonFpPoints + 6
+} else if (numberInHousehold === 6) {
+    carbonFpPoints = carbonFpPoints + 4
+} else if (numberInHousehold > 6) {
+    carbonFpPoints = carbonFpPoints + 2
+}  else {
+    console.log("no update to points")
+}
+console.log(`Points for household of ${numberInHousehold} would be ${carbonFpPoints} `)
