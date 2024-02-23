9d848e5ca31aa88c89576918a97d9f1846fab73e
Gio559 <joba2017@outlook.com>
2024-02-22 23:29:17 -0800

    my attempt at the code, before I review other students code

--- w7/main.js
+++ w7/main.js
@@ -1,43 +1,10 @@
 import { renderTbl } from "./render.js";
+import { determineHouseHoldPts,determineHouseSizePts } from "./other.js";
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
 
-let cardonFootprintPoints = 0;
 
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large")  {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 6;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
-  }
-  return houseSizePoints;
-}
-
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPts = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPts = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPts = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPts = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPts = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPts = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPts = 4;
-  } else {
-    houseHoldPts = 2;
-  }
-  return houseHoldPts;
-}
 
 function start(first, last, houseHoldMembers, houseHoldSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
