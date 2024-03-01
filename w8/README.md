7c7f7f551b0e1202c565f896f81427da938519b4
Gio559 <joba2017@outlook.com>
2024-02-29 23:21:46 -0800

    starting code ready

---
+++ w8/README.md
@@ -0,0 +1,5 @@
+What was e.preventdefault?
+Itd be better to view through vscpde
+Good review, was lost in some areas so this is helpful
+Maybe more info in append child and arrays
+Add another event listener
---
+++ w8/main.js
@@ -0,0 +1,33 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./other.js";
+
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+const cfpData = [];
+
+function start(firstName, lastName, houseHoldMembers, houseHoldSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePTS = determineHouseSizePts(houseHoldSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push({
+    firstName: firstName,
+    lastName: lastName,
+    houseHold: houseHoldMembers,
+    houseSPTS: houseSizePTS,
+    houseSize: houseHoldSize,
+    cfpTotal: total,
+  });
+}
+
+FORM.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const houseMembers = parseInt(FORM.housem.value);
+  const houseSize = FORM.houses.value;
+  start(firstName, lastName, houseMembers, houseSize); // Pass all four arguments to start function
+  OUTPUT.innerHTML = "";
+  renderTbl(cfpData);
+  FORM.reset();
+});
+// no questions on modules, reading the mdn doc. I had an error bc I had both fun in the modules
