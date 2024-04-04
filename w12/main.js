ad13006d869ce612d4c122fc07f85c315086fb45
Gio559 <joba2017@outlook.com>
2024-04-03 22:25:09 -0700

    w12 my attempt at the code, before I review other students code

---
+++ w12/main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form")
+ const listener = addEventListener("submit", function (event) {
+    event.preventDefault();
+    const exercise = document.getElementById("exercise").value;
+    const sets = document.getElementById("sets").value;
+    const time = document.getElementById("time").value;
+      if (exercise && sets && time) {
+        const totalTime = sets * time * 60 * 10;
+        const messageDiv = document.createElement("div");
+      document.body.appendChild(messageDiv);
+        const startExercise = () => {
+            messageDiv.textContent = `Start your ${exercise}.`;
+        };
+        const stopExercise = () => {
+            messageDiv.textContent = `No more ${exercise}.`;
+        };
+        startExercise();
+        setTimeout(stopExercise, totalTime);
+    } else {
+        messageDiv.textContent = "Please fill out the form.";
+    }
+});
