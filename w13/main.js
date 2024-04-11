e799296bd83c30308447711939f093ca4b87819a
Gio559 <joba2017@outlook.com>
2024-04-11 00:08:20 -0700

    w13 my attempt at the code, before I review other students or solution code.

---
+++ w13/main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form");
+
+const startExercise = (exercise, messageDiv) => {
+  if (exercise && messageDiv) {
+    messageDiv.textContent = `Start your ${exercise}.`;
+    return Promise.resolve();
+  } else {
+    return Promise.reject("Exercise is not defined");
+  }
+};
+
+const stopExercise = (exercise, messageDiv) => {
+  if (exercise && messageDiv) {
+    messageDiv.textContent = `Stop your ${exercise}.`;
+    return Promise.resolve();
+  } else {
+    return Promise.reject("Exercise  is not defined");
+  }
+};
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const exercise = document.getElementById("exercise").value;
+  const sets = document.getElementById("sets").value;
+  const time = document.getElementById("time").value;
+
+  if (exercise && sets && time) {
+      const totalTime = sets * time * 60 * 10;
+      const messageDiv = document.createElement("div");
+      document.body.appendChild(messageDiv);
+
+      startExercise(exercise, messageDiv)
+          .then(() => {
+              setTimeout(() => {
+                  stopExercise(exercise, messageDiv)
+                      .catch((error) => {
+                          console.log("An error occurred");
+                      });
+              }, totalTime);
+          })
+          .catch((error) => {
+            console.log( "An error occurred.")
+          });
+  } else {
+      console.log("Please fill out the form.");
+  }
+});
