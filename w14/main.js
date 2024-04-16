3e43fe8bac1b6de8aa368478d58fb848ddcf159b
Gio559 <joba2017@outlook.com>
2024-04-15 22:39:30 -0700

    my code for API challenge

--- w14/main.js
+++ w14/main.js
@@ -23,7 +23,7 @@ console.log(result.properties.periods[1].shortForecast)
 
  async function start() {
     try{
-        const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+        const data = await fetch('https://api.weather.gov/gridpoints/HNX/52,100/forecast')
         const result =  await data.json();
         onSuccess(result.properties.periods[1].shortForecast)
     } catch (e) {
