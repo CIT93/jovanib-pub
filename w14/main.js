b541fe57e5e192d181ef390ca9527c90b1e3498d
Gio559 <joba2017@outlook.com>
2024-04-15 22:21:25 -0700

    try/catch with api call

--- w14/main.js
+++ w14/main.js
@@ -13,21 +13,28 @@ console.log(result.properties.periods[1].shortForecast)
      }
  start() */
  
- function getData() {
+ /* function getData() {
     return new Promise(function(resolve, reject) {
         setTimeout(() => {
             reject('Something went wrong.')
         }, 1)
     })
- }
+ } */
 
  async function start() {
     try{
-        const result = await getData()
-       //Success
-    } catch (error) {
-        //Failure
+        const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+        const result =  await data.json();
+        onSuccess(result.properties.periods[1].shortForecast)
+    } catch (e) {
+onError(e);
     }
- }
+    }
+ function onSuccess(result){
+console.log(`Success: $(result`)
+}
+function onError(err){
+console.log(`Error: $(err`)
+} 
 
  start()
